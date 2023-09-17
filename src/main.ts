366e6583e38b8d9f65ddbccf525bf47906a2c349
Tommaso Ricci <zorgatone@gmail.com>
2023-09-17 19:27:27 +0200

    Moved to Svelte

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-import backgroundWebWorker from "./worker/background?worker&url";
-import serviceWorker from "./worker/service?worker&url";
+import "./app.css";
+import App from "./App.svelte";
 
-// import {} from 'snarkdown'
-// import {} from 'xss'
-// import {} from 'idb'
-// import {} from 'redaxios'
-// import {} from 'preact'
-
-const serviceWorkerEnabled: boolean = false;
-
-if (serviceWorkerEnabled && "serviceWorker" in navigator) {
-  navigator.serviceWorker.register(serviceWorker, { type: "module" });
-}
-
-console.log(backgroundWebWorker, import.meta.url);
-const w = new Worker(new URL(backgroundWebWorker, import.meta.url), {
-  type: "module",
+const app = new App({
+  target: document.getElementById("app") as HTMLDivElement,
 });
 
-console.log("Hello, Vite!");
-
-w.postMessage("testStorage");
+export default app;
